dedf6fbe2b49fb5af32e7c0a95dcf43c1da5c232
ktakada42 <ktakada950711@gmail.com>
2022-05-24 23:38:34 +0900

    fix: free_roughly

--- ft_printf.c
+++ ft_printf.c
@@ -35,69 +35,45 @@ int	ft_printf(const char *fmt, ...)
 			{
 				arg = ft_ctos(va_arg(ap, int));
 				print_count = ft_printstr(arg, print_count);
+				free(arg);
 			}
 			else if (*fmt == 's')
 			{
 				arg = va_arg(ap, char *);
-				while (*arg != '\0')
-				{
-					write(1, arg, 1);
-					print_count++;
-					arg++;
-				}
+				print_count = ft_printstr(arg, print_count);
 			}
 			else if (*fmt == 'p')
 			{
 				address = (intptr_t)va_arg(ap, void *);
 				arg = ft_itoa_base(address, 16);
 				arg = ft_join_address(arg);
-				while (*arg != '\0')
-				{
-					write(1, arg, 1);
-					print_count++;
-					arg++;
-				}
+				print_count = ft_printstr(arg, print_count);
+				free(arg);
 			}
 			else if (*fmt == 'd' || *fmt == 'i')
 			{
 				arg = ft_itoa_base(va_arg(ap, int), 10);
-				while (*arg != '\0')
-				{
-					write(1, arg, 1);
-					print_count++;
-					arg++;
-				}
+				print_count = ft_printstr(arg, print_count);
+				free(arg);
 			}
 			else if (*fmt == 'u')
 			{
 				arg = ft_uitoa(va_arg(ap, unsigned int));
-				while (*arg != '\0')
-				{
-					write(1, arg, 1);
-					print_count++;
-					arg++;
-				}
+				print_count = ft_printstr(arg, print_count);
+				free(arg);
 			}
 			else if (*fmt == 'x')
 			{
 				arg = ft_itoa_base(va_arg(ap, int), 16);
-				while (*arg != '\0')
-				{
-					write(1, arg, 1);
-					print_count++;
-					arg++;
-				}
+				print_count = ft_printstr(arg, print_count);
+				free(arg);
 			}
 			else if (*fmt == 'X')
 			{
 				arg = ft_itoa_base(va_arg(ap, int), 16);
 				arg = ft_toupper_string(arg);
-				while (*arg != '\0')
-				{
-					write(1, arg, 1);
-					print_count++;
-					arg++;
-				}
+				print_count = ft_printstr(arg, print_count);
+				free(arg);
 			}
 			else if (*fmt == '%')
 			{
@@ -115,38 +91,38 @@ int	ft_printf(const char *fmt, ...)
 	return print_count;
 }
 
-#include <stdio.h>
+/* #include <stdio.h> */
 
-int	main(void)
-{
-	int	  ret = 0;
-	char  *str = "Hello";
+/* int	main(void) */
+/* { */
+/* 	int	  ret = 0; */
+/* 	char  *str = "Hello"; */
 
-	ft_printf("%c, %c, %c, %%\n", 'a', 'b', 'c');
-	ft_printf("%s, world!\n", "hello");
-	ft_printf("ft: %p\n", &ret);
-	printf("or: %p\n", &ret);
-	ft_printf("ft: %p\n", str);
-	printf("or: %p\n", str);
-	ft_printf("ft: %p\n", &str);
-	printf("or: %p\n", &str);
-	ft_printf("%d\n", 0);
-	ft_printf("%i\n", 0);
-	ft_printf("%d\n", 2147483647);
-	ft_printf("%i\n", 2147483647);
-	ft_printf("%d\n", -2147483648);
-	ft_printf("%i\n", -2147483648);
-	ft_printf("%u\n", 0);
-	ft_printf("4294967295: %u\n", -1);
-	ft_printf("%u\n", 2147483647);
-	ft_printf("2147483648: %u\n", -2147483648);
-	ft_printf("%u\n", 4294967295);
-	ft_printf("%x\n", 0);
-	ft_printf("%X\n", 0);
-	ft_printf("%x\n", 2147483647);
-	ft_printf("%X\n", 2147483647);
-	ft_printf("%x\n", -2147483648);
-	ft_printf("%X\n", -2147483648);
-	system("leaks -q a.out");
-	return (0);
-}
+/* 	ft_printf("%c, %c, %c, %%\n", 'a', 'b', 'c'); */
+/* 	ft_printf("%s, world!\n", "hello"); */
+/* 	ft_printf("ft: %p\n", &ret); */
+/* 	printf("or: %p\n", &ret); */
+/* 	ft_printf("ft: %p\n", str); */
+/* 	printf("or: %p\n", str); */
+/* 	ft_printf("ft: %p\n", &str); */
+/* 	printf("or: %p\n", &str); */
+/* 	ft_printf("%d\n", 0); */
+/* 	ft_printf("%i\n", 0); */
+/* 	ft_printf("%d\n", 2147483647); */
+/* 	ft_printf("%i\n", 2147483647); */
+/* 	ft_printf("%d\n", -2147483648); */
+/* 	ft_printf("%i\n", -2147483648); */
+/* 	ft_printf("%u\n", 0); */
+/* 	ft_printf("4294967295: %u\n", -1); */
+/* 	ft_printf("%u\n", 2147483647); */
+/* 	ft_printf("2147483648: %u\n", -2147483648); */
+/* 	ft_printf("%u\n", 4294967295); */
+/* 	ft_printf("%x\n", 0); */
+/* 	ft_printf("%X\n", 0); */
+/* 	ft_printf("%x\n", 2147483647); */
+/* 	ft_printf("%X\n", 2147483647); */
+/* 	ft_printf("%x\n", -2147483648); */
+/* 	ft_printf("%X\n", -2147483648); */
+/* 	system("leaks -q a.out"); */
+/* 	return (0); */
+/* } */
