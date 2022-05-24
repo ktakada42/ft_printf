7d7cba9548b7da7a0ab87c5eb99dfac084887cab
Ktaka95 <ktakada950711@gmail.com>
2022-05-24 15:44:43 +0900

    feat: %s, %c can print

--- ft_printf.c
+++ ft_printf.c
@@ -1,12 +1,11 @@
-/* ************************************************************************** */
-/*                                                                            */
+/* ************************************************************************** */ /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ktakada <ktakada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/21 15:35:20 by ktakada           #+#    #+#             */
-/*   Updated: 2022/05/22 00:07:26 by ktakada          ###   ########.fr       */
+/*   Updated: 2022/05/24 15:41:56 by ktakada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +15,9 @@ int	ft_printf(const char *fmt, ...)
 {
 	va_list	ap;
 	int		print_count;
-	t_flag	*f;
+	char	arg_c;
+	char	*arg_s;
+	/* int		arg_i; */
 
 	va_start(ap, fmt);
 	print_count = 0;
@@ -26,10 +27,36 @@ int	ft_printf(const char *fmt, ...)
 		{
 			write(1, fmt, 1);
 			print_count++;
-			fmt++;
 		}
 		else
-			ft_parse_fmt(fmt, &print_count, ap, &f);
+		{
+			fmt++;
+			if (*fmt == 'c')
+			{
+				arg_c = (char)va_arg(ap, int);
+				write(1, &arg_c, 1);
+				print_count++;
+			}
+			else if (*fmt == 's')
+			{
+				arg_s = va_arg(ap, char *);
+				while (*arg_s != '\0')
+				{
+					write(1, arg_s, 1);
+					print_count++;
+					arg_s++;
+				}
+			}
+			/* else if (*fmt == 'd' || *fmt == 'u') */
+			/* { */
+			/* 	arg_i = va_arg(ap, int); */
+			/* } */
+			else
+			{
+				return (-1);
+			}
+		}
+		fmt++;
 	}
 	va_end(ap);
 	return print_count;
@@ -41,7 +68,9 @@ int	main(void)
 {
 	int	ret;
 
-	ret = printf("%d\n%s\n", 1, "hello");
+	ret = printf("%c, %c, %c\n", 'a', 'b', 'c');
+	printf("%d\n", ret);
+	ret = printf("%s, world!\n", "hello");
 	printf("%d\n", ret);
 	return (0);
 }
