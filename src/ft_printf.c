09bfbfe7f6bb07fca2e3dda05f5587f15b7938d2
Ktaka95 <ktakada950711@gmail.com>
2022-05-27 18:15:53 +0900

    test: %s_NULL_case

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: ktakada <ktakada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/21 15:35:20 by ktakada           #+#    #+#             */
-/*   Updated: 2022/05/27 18:06:16 by ktakada          ###   ########.fr       */
+/*   Updated: 2022/05/27 18:15:39 by ktakada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -121,77 +121,11 @@ int	print_spec_xX(va_list *ap, int *put_count, const char *fmt)
 	free (tmp_to_free);
 	return (*put_count);
 }
-/* #include <stdio.h> */
 
-/* int	main(void) */
-/* { */
-/* 	int	*p; */
-/* 	int	*q; */
-/* 	p = (int*)-1; */
+#include <stdio.h>
+#include <assert.h>
 
-/* 	printf("or: %p \n", p); */
-/* 	ft_printf("ft: %p \n", p); */
-/* 	p = (int*)1; */
-/* 	printf("or: %p \n", p); */
-/* 	ft_printf("ft: %p \n", p); */
-/* 	p = (int*)15; */
-/* 	printf("or: %p \n", p); */
-/* 	ft_printf("ft: %p \n", p); */
-/* 	p = (int*)16; */
-/* 	printf("or: %p \n", p); */
-/* 	ft_printf("ft: %p \n", p); */
-/* 	p = (int*)17; */
-/* 	printf("or: %p \n", p); */
-/* 	ft_printf("ft: %p \n", p); */
-/* 	p = (int*)LONG_MIN; */
-/* 	q = (int*)LONG_MAX; */
-/* 	printf("or: %p %p \n", p, q); */
-/* 	ft_printf("ft: %p %p \n", p, q); */
-/* 	p = (int*)INT_MIN; */
-/* 	q = (int*)INT_MAX; */
-/* 	printf("or: %p %p \n", p, q); */
-/* 	ft_printf("ft: %p %p \n", p, q); */
-/* 	p = (int*)ULONG_MAX; */
-/* 	q = (int*)-ULONG_MAX; */
-/* 	printf("or: %p %p \n", p, q); */
-/* 	ft_printf("ft: %p %p \n", p, q); */
-/* 	p = (int*)0; */
-/* 	q = (int*)0; */
-/* 	printf("or: %p %p \n", p, q); */
-/* 	ft_printf("ft: %p %p \n", p, q); */
-/* } */
-
-/* 	char  *a = NULL; */
-/* 	int	  ret = 0; */
-/* 	char  *str = "Hello"; */
-
-/* 	ft_printf("%c, %c, %c, %%\n", 'a', 'b', 'c'); */
-/* 	ft_printf("%s, world!\n", "hello"); */
-/* 	ft_printf("%s\n", NULL); */
-/* 	printf("%s\n", a); */
-/* 	ft_printf("ft: %p\n", &ret); */
-/* 	printf("or: %p\n", &ret); */
-/* 	ft_printf("ft: %p\n", str); */
-/* 	printf("or: %p\n", str); */
-/* 	ft_printf("ft: %p\n", &str); */
-/* 	printf("or: %p\n", &str); */
-/* 	ft_printf("%d\n", 0); */
-/* 	ft_printf("%i\n", 0); */
-/* 	ft_printf("%d\n", 2147483647); */
-/* 	ft_printf("%i\n", 2147483647); */
-/* 	ft_printf("%d\n", -2147483648); */
-/* 	ft_printf("%i\n", -2147483648); */
-/* 	ft_printf("%u\n", 0); */
-/* 	ft_printf("4294967295: %u\n", -1); */
-/* 	ft_printf("%u\n", 2147483647); */
-/* 	ft_printf("2147483648: %u\n", -2147483648); */
-/* 	ft_printf("%u\n", 4294967295); */
-/* 	ft_printf("%x\n", 0); */
-/* 	ft_printf("%X\n", 0); */
-/* 	ft_printf("%x\n", 2147483647); */
-/* 	ft_printf("%X\n", 2147483647); */
-/* 	ft_printf("%x\n", -2147483648); */
-/* 	ft_printf("%X\n", -2147483648); */
-/* 	system("leaks -q a.out"); */
-/* 	return (0); */
-/* } */
+int	main(void)
+{
+	assert (printf("%s\n", NULL) == ft_printf("%s\n", NULL));
+}
